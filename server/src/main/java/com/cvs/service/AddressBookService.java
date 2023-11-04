749ac13e1ef5608bfb39d0d532e6c1aa3c995468
CVinit <vv598@outlook.com>
2023-11-04 23:52:05 +0800

    地址簿管理代码

---
+++ server/src/main/java/com/cvs/service/AddressBookService.java
@@ -0,0 +1,20 @@
+package com.cvs.service;
+
+import com.cvs.entity.AddressBook;
+import java.util.List;
+
+public interface AddressBookService {
+
+    List<AddressBook> list(AddressBook addressBook);
+
+    void save(AddressBook addressBook);
+
+    AddressBook getById(Long id);
+
+    void update(AddressBook addressBook);
+
+    void setDefault(AddressBook addressBook);
+
+    void deleteById(Long id);
+
+}
