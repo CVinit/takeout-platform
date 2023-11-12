7cf0771428606f1cdbc65d61f05e54ff37e72908
CVinit <vv598@outlook.com>
2023-11-12 23:27:11 +0800

    导出excel文件代码实现

--- server/src/main/java/com/cvs/service/ReportService.java
+++ server/src/main/java/com/cvs/service/ReportService.java
@@ -5,6 +5,7 @@ import com.cvs.vo.SalesTop10ReportVO;
 import com.cvs.vo.TurnoverReportVO;
 import com.cvs.vo.UserReportVO;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 public interface ReportService {
@@ -37,4 +38,10 @@ public interface ReportService {
      * @return
      */
     SalesTop10ReportVO getSalesTop10(LocalDate begin, LocalDate end);
+
+    /**
+     * 导出运营数据报表
+     * @param response
+     */
+    void exportBusinessData(HttpServletResponse response);
 }
